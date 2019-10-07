f3b07e984b593214b2fed7591bd64b9b36d8d00c
aprado94 <aprado@bilbomatica.es>
2019-10-07 08:05:21 +0200

    Fix an issue where european bar charts non eu line wasnot correctly painting.

--- static/custom/modules/vertical/survey/detailpage/DetailPageController.js
+++ static/custom/modules/vertical/survey/detailpage/DetailPageController.js
@@ -292,7 +292,7 @@ define(function (require) {
 		        color5: dvtUtils.getColorCountry(2),
 		        color6: dvtUtils.getColorCountry(12),
 		        color7: dvtUtils.getColorCountry(4),
-				plots: DetailPageService.getGeneralEuropeanBarCharPlot($scope.dashboard.parameters.pFilters.euOnly, $scope.answer),
+				plots: DetailPageService.getGeneralEuropeanBarCharPlot($scope.dashboard.parameters.pFilters.euOnly, /*$scope.answer*/0),
 				dimensions: {
 				  	value: {
 						format: {
