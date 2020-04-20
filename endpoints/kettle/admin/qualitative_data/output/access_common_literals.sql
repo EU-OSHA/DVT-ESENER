88d8440b8718c4f0f076cc568ed2b12c7e88bd37
dprado002 <dprado@bilbomatica.es>
2020-04-20 08:18:21 +0200

    ESN-1 - Loaded latest qualitative database

--- endpoints/kettle/admin/qualitative_data/output/access_common_literals.sql
+++ endpoints/kettle/admin/qualitative_data/output/access_common_literals.sql
@@ -8,13 +8,3 @@ INSERT INTO literal (id, chart_id, section_id, type) VALUES (@literalId, null, n
 INSERT INTO translation (literal_id, language, is_default, text) VALUES (@literalId, "EN", 1, "<p>Date of access to the website.</p><p><em>Privacy statement last updated: </em></p>");
 
 
-SET @literalId=(SELECT (SELECT MAX(l.id) AS maxID FROM literal l WHERE l.id < 10000)+1);
-INSERT INTO literal (id, chart_id, section_id, type) VALUES (@literalId, null, null, "PRIVACY_NOTICE");
-INSERT INTO translation (literal_id, language, is_default, text) VALUES (@literalId, "EN", 1, "<p><strong>Date when processing starts</strong></p>");
-
-
-SET @literalId=(SELECT (SELECT MAX(l.id) AS maxID FROM literal l WHERE l.id < 10000)+1);
-INSERT INTO literal (id, chart_id, section_id, type) VALUES (@literalId, null, null, "PRIVACY_NOTICE");
-INSERT INTO translation (literal_id, language, is_default, text) VALUES (@literalId, "EN", 1, "<p>Date of access to the website.</p><p><em>Privacy statement last updated: </em></p>");
-
-
